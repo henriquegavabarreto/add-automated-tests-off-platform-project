bb40a54a54ed04a0717c363eda7dd8210f98c558
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-24 11:28:06 -0700

    fix deposit test

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,7 +11,7 @@ def test_deposit(app, client):
     del app
     res = client.get('/deposit?amount=500')
     assert res.status_code == 200
-    expected = {'balance': 50}
+    expected = {'balance': 500}
     assert expected == json.loads(res.get_data(as_text=True))
 
 def test_deposit_2(app, client):
